22e6cc5d13baa14643d2c8242c7389290c88240a
RahulPalXDA <RahulPal.XDA@gmail.com>
2023-02-13 12:18:49 +0530

    adding my sway install script

---
+++ sway.sh
@@ -0,0 +1,10 @@
+base_packages() {
+	sudo pacman -S sway wayland foot swayimg swaylock grim brightnessctl pulseaudio pulseaudio-alsa alsa-utils wofi swaybg
+}
+setup_tty() {
+	echo "if [ "$(tty)" = "/dev/tty1" ]; then" >> ~/.bash_profile
+	echo "	exec dbus-run-session sway" >> ~/.bash_profile
+	echo "fi" >> ~/.bash_profile
+}
+base_packages
+setup_tty
