5729dd12fb6ecc8370045726f918419ee2afe15f
Rahul Pal XDA <RahulPal.XDA@gmail.com>
2023-02-17 00:00:41 +0530

    sway: force firefox to use wayland

--- sway.sh
+++ sway.sh
@@ -2,6 +2,9 @@ base_packages() {
 	sudo pacman -S sway wayland foot swayimg swaylock grim brightnessctl pulseaudio pulseaudio-alsa alsa-utils wofi swaybg libnotify dunst
 }
 setup_tty() {
+	echo "" >> ~/.bashrc
+	echo "# Force firefox to wayland" >> ~/.bashrc
+	echo "export MOZ_ENABLE_WAYLAND=1" >> ~/.bashrc
 	echo "if [ "$(tty)" = "/dev/tty1" ]; then" >> ~/.bash_profile
 	echo "	exec dbus-run-session sway" >> ~/.bash_profile
 	echo "fi" >> ~/.bash_profile
