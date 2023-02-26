5ff0c38657a27806a7c69b35b14fffbe89dba351
RahulPalXDA <RahulPal.XDA@gmail.com>
2023-02-26 16:27:51 +0530

    initial personal setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+config_git() {
+	git config --global user.name "RahulPalXDA"
+	git config --global user.email "RahulPal.XDA@gmail.com"
+	git config --global core.editor nvim
+}
+install_utilities() {
+	sudo pacman -Syy firefox mpv ranger moc clang android-tools
+	git clone --depth=1 https://aur.archlinux.org/yay.git ~/Desktop/yay
+	cd ~/Desktop/yay/ && makepkg -si
+	cd ~/Desktop/ && rm -rf yay
+	yay -S cava
+}
+config_git
+install_utilities
