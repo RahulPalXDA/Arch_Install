ceec4f4d30b9f39871b3821fa87ae8529d7df2a1
Rahul Pal XDA <RahulPal.XDA@gmail.com>
2023-02-26 12:22:36 +0530

    fixup: move MOZ_ENABLE_WAYLAND flag to bash_profile

--- sway.sh
+++ sway.sh
@@ -2,9 +2,10 @@ base_packages() {
 	sudo pacman -S sway wayland foot swayimg swaylock grim brightnessctl pulseaudio pulseaudio-alsa alsa-utils wofi swaybg libnotify dunst
 }
 setup_tty() {
-	echo "" >> ~/.bashrc
-	echo "# Force firefox to wayland" >> ~/.bashrc
-	echo "export MOZ_ENABLE_WAYLAND=1" >> ~/.bashrc
+	echo "" >> ~/.bash_profile
+	echo "# Force firefox to wayland" >> ~/.bash_profile
+	echo "export MOZ_ENABLE_WAYLAND=1" >> ~/.bash_profile
+	echo "" >> ~/.bash_profile
 	echo "if [ "$(tty)" = "/dev/tty1" ]; then" >> ~/.bash_profile
 	echo "	exec dbus-run-session sway" >> ~/.bash_profile
 	echo "fi" >> ~/.bash_profile
